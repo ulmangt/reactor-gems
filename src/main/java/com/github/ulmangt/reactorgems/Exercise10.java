fb1d1a1f950521515c2de677b33dc6c153cc8535
Geoffrey Ulman <ulman@metsci.com>
2022-11-14 00:34:49 -0500

    Add Context exercise

---
+++ src/main/java/com/github/ulmangt/reactorgems/Exercise10.java
@@ -0,0 +1,14 @@
+package com.github.ulmangt.reactorgems;
+
+import reactor.core.publisher.Mono;
+
+/**
+ * Exercise 10 - Context Propagation
+ *
+ * @see <a href="https://projectreactor.io/docs/core/release/reference/#context">Project Reactor Context Documentation</a>
+ * @see <a href="https://github.com/micrometer-metrics/context-propagation">Micrometer Context-Propagation GitHub Project</a>
+ * @see <a href="https://domenicosibilio.medium.com/the-good-the-bad-and-the-ugly-of-propagating-data-through-reactive-streams-62beac48789e">Reactor Context Example Blog Post</a>
+ */
+public class Exercise10
+{
+}
